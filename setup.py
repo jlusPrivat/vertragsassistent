47dcd50524264f850b8d43420d8e50703d2619b1
Jonathan Lusky <51421005+jlusPrivat@users.noreply.github.com>
2025-04-19 09:32:17 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='vertragsassistent',
+    version='1.0.0',
+    packages=[''],
+    url='jlus.de',
+    license='',
+    author='jl',
+    author_email='',
+    description='',
+    install_requires=[
+        'PySide6', 'peewee'
+    ]
+)
